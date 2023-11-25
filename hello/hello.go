519d7503069283a45538d7884daac62f6ed418eb
Sriman S <55436171+swamisriman@users.noreply.github.com>
2023-11-26 01:44:27 +0530

    Modified the hello function to accept a name

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello World"
+// func Hello() string {
+// 	return "Hello World"
+// }
+
+func Hello(person_name string) string {
+	return "Hello " + person_name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Sriman"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -6,8 +6,17 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	exp := "Hello World"
-	obs := Hello()
+	// exp := "Hello World"
+	// obs := Hello()
+
+	// if exp == obs {
+	// 	fmt.Println("All good")
+	// } else {
+	// 	t.Errorf("Obtained: %q but Expected: %q", obs, exp)
+	// }
+
+	obs := Hello("Sriman")
+	exp := "Hello Sriman"
 
 	if exp == obs {
 		fmt.Println("All good")
