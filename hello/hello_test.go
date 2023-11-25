94503c1986ba8c704d18bae14fefbec636ef9bc8
Sriman S <55436171+swamisriman@users.noreply.github.com>
2023-11-26 01:29:10 +0530

    Added 'Hello World'

---
+++ hello/hello_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	exp := "Hello World"
+	obs := Hello()
+
+	if exp == obs {
+		fmt.Println("All good")
+	} else {
+		t.Errorf("Obtained: %q but Expected: %q", obs, exp)
+	}
+}
