50b769b76dd092efd34e78069ac8025408bca9fe
Sriman S <55436171+swamisriman@users.noreply.github.com>
2023-12-18 02:42:43 +0530

    Added tail summer

--- summer/summer.go
+++ summer/summer.go
@@ -7,3 +7,23 @@ func Sum(arr []int) int {
 	}
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	var sums []int
+	for _, slice := range slices {
+		sums = append(sums, Sum(slice))
+	}
+	return sums
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		sums = append(sums, Sum(slice[1:]))
+	}
+	return sums
+}
--- summer/summer_test.go
+++ summer/summer_test.go
@@ -1,24 +1,52 @@
 package summer
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	arr := []int{1, 2, 3, 4, 5}
-
-	expectedSum := 15
-	actualSum := Sum(arr)
-
-	if expectedSum != actualSum {
-		t.Errorf("Expected: %d but Actual: %d. Given array %v", expectedSum, actualSum, arr)
-	}
-
 	t.Run("any size", func(t *testing.T) {
 		arr := []int{1, 2, 3, 4}
 		expectedSum := 10
 		actualSum := Sum(arr)
 
 		if expectedSum != actualSum {
-			t.Errorf("Expected: %d but Actual: %d. Given array %v", expectedSum, actualSum, arr)
+			t.Errorf("Expected: %d but Actual: %d. Given: %v", expectedSum, actualSum, arr)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	expectedSums := []int{15, 13}
+	slicesInput := [][]int{{1, 2, 3, 4, 5}, {6, 7}}
+	actualSums := SumAll(slicesInput[0], slicesInput[1])
+
+	if !slices.Equal(expectedSums, actualSums) {
+		t.Errorf("Expected: %v but Actual: %d. Given: %v", expectedSums, actualSums, slicesInput)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertSlicesEquality := func(t testing.TB, expected, actual []int) {
+		t.Helper()
+		if !slices.Equal(expected, actual) {
+			t.Errorf("Expected: %v but Actual: %d", expected, actual)
+		}
+	}
+	t.Run("Tails Sum, non-empty slice", func(t *testing.T) {
+		expectedSums := []int{14, 7}
+		slices := [][]int{{1, 2, 3, 4, 5}, {6, 7}}
+		actualSums := SumAllTails(slices[0], slices[1])
+
+		assertSlicesEquality(t, expectedSums, actualSums)
+	})
+
+	t.Run("Tails Sum, with empty slice", func(t *testing.T) {
+		expectedSums := []int{14, 0}
+		slices := [][]int{{1, 2, 3, 4, 5}, {}}
+		actualSums := SumAllTails(slices[0], slices[1])
+
+		assertSlicesEquality(t, expectedSums, actualSums)
 	})
 }
