9a2adc6b6e67175fb245090bf0ec865c71d601c6
Sriman S <55436171+swamisriman@users.noreply.github.com>
2024-02-05 01:31:40 +0530

    Added ping method for racing websites Added timeout for website racer

--- sync_demon/sync_demon.go
+++ sync_demon/sync_demon.go
@@ -1,40 +1,32 @@
 package sync_demon
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-type websiteDuration struct {
-	string
-	duration time.Duration
-}
-
-func RaceWebsites(siteA, siteB string) string {
-
-	durationChannel := make(chan websiteDuration)
-
-	go GetWebsiteDuration(siteA, durationChannel)
-	go GetWebsiteDuration(siteB, durationChannel)
-
-	durations := make(map[string]time.Duration)
-
-	dur := <-durationChannel
-	durations[dur.string] = dur.duration
-	dur = <-durationChannel
-	durations[dur.string] = dur.duration
-
-	if durations[siteA] < durations[siteB] {
-		return siteA
-	} else {
-		return siteB
+func ConfigurableRaceWebsites(siteA, siteB string, delay time.Duration) (string, error) {
+	select {
+	case <-ping(siteA):
+		return siteA, nil
+	case <-ping(siteB):
+		return siteB, nil
+	case <-time.After(delay):
+		return "", fmt.Errorf("timed out waiting for %s and %s", siteA, siteB)
 	}
 }
 
-func GetWebsiteDuration(url string, durChan chan (websiteDuration)) {
-	start := time.Now()
-	http.Get(url)
-	duration := time.Since(start)
+func ping(url string) chan struct{} {
+	tempChannel := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(tempChannel)
+	}()
 
-	durChan <- websiteDuration{url, duration}
+	return tempChannel
+}
+
+func DefaultRaceWebsites(siteA, siteB string) (string, error) {
+	return ConfigurableRaceWebsites(siteA, siteB, 10*time.Second)
 }
--- sync_demon/sync_demon_test.go
+++ sync_demon/sync_demon_test.go
@@ -8,18 +8,40 @@ import (
 )
 
 func TestRaceWebsites(t *testing.T) {
-	slowServer := GetDelayedTestServer(10 * time.Millisecond)
-	fastServer := GetDelayedTestServer(0 * time.Millisecond)
+	t.Run("Durations checker", func(t *testing.T) {
+		slowServer := GetDelayedTestServer(20 * time.Millisecond)
+		fastServer := GetDelayedTestServer(0 * time.Millisecond)
 
-	expected := fastServer.URL
-	actual := RaceWebsites(slowServer.URL, fastServer.URL)
+		expected := fastServer.URL
+		actual, err := ConfigurableRaceWebsites(slowServer.URL, fastServer.URL, 1*time.Second)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	if expected != actual {
-		t.Errorf("expected: %s, but actual: %s", expected, actual)
-	}
+		if err != nil {
+			t.Error("expected: no time out, but actual: timed out")
+		}
+		if expected != actual {
+			t.Errorf("expected: %s, but actual: %s", expected, actual)
+		}
+	})
+
+	t.Run("Timeout checker", func(t *testing.T) {
+		slowServer := GetDelayedTestServer(3 * time.Second)
+		fastServer := GetDelayedTestServer(2 * time.Second)
+
+		actual, err := ConfigurableRaceWebsites(slowServer.URL, fastServer.URL, 2*time.Second)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		if err == nil {
+			t.Errorf("expected: time out, but actual: not timed out")
+		}
+		if actual != "" {
+			t.Errorf("expected: %q, but actual: %q", "", actual)
+		}
+	})
 }
 
 func GetDelayedTestServer(delay time.Duration) *httptest.Server {
